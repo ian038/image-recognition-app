054476c6d2c5ce6114afbfd0db1860e74167f6b8
ian038 <ianphua3@gmail.com>
2021-12-16 13:18:20 -0500

    Added particles effect and rank

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+        params={particlesOptions}
+      />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecogniton /> */}
     </div>
