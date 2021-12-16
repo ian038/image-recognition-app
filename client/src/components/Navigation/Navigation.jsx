38e1fc0d789cf626e536d7f9d1d55f3259bdf53c
ian038 <ianphua3@gmail.com>
2021-12-16 12:41:29 -0500

    Added navigation and completed logo

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Navigation() {
+export default function Navigation({ onRouteChange, isSignedIn }) {
     return (
-        <div>
-            Navigation
-        </div>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        </nav>
     )
 }
