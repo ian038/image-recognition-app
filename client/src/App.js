b27cf18bc0d59dd191835e850d0e49453849a4f5
ian038 <ianphua3@gmail.com>
2021-12-16 11:24:11 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/* <Logo />
+      <ImageLinkForm />
+      <FaceRecogniton /> */}
+    </div>
+  );
+}
+
+export default App;
