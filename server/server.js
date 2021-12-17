c0c67b217d137126e5c7d7cf10d6090d081c2a5d
ian038 <ianphua3@gmail.com>
2021-12-17 16:13:24 -0500

    Refactored code for better readability

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const bcrypt = require('bcrypt');
 const cors = require('cors');
 const knex = require('knex')
 
+const auth = require('./controllers/auth');
+const image = require('./controllers/image');
+
 const app = express();
 
 app.use(cors())
@@ -18,62 +21,11 @@ const db = knex({
     }
 });
 
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-      .where('email', '=', req.body.email)
-      .then(data => {
-        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if (isValid) {
-          return db.select('*').from('users')
-            .where('email', '=', req.body.email)
-            .then(user => {
-              res.json(user[0])
-            })
-            .catch(err => res.status(400).json('unable to get user'))
-        } else {
-          res.status(400).json('wrong credentials')
-        }
-    }).catch(err => res.status(400).json('wrong credentials'))
-})
+app.post('/signin', auth.signIn(db, bcrypt))
   
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password, 12);
-    db.transaction(trx => {
-        trx.insert({
-          hash: hash,
-          email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
-            .insert({
-              email: loginEmail[0],
-              name: name,
-              joined: new Date()
-            })
-            .then(user => {
-              res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    }).catch(err => {
-        res.status(400).json('unable to register')
-    })
-})
+app.post('/register', (req, res) => { auth.register(req, res, db, bcrypt) })
   
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    }).catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(8000, ()=> {
     console.log('app running on port 8000');
